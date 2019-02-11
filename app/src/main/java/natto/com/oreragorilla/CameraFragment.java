94e4cd54b415c7247bd3d21487ccc4441894790c
TaigaNatto <taigan3253@gmail.com>
2019-02-11 15:28:54 +0900

    add CameraFragment

---
+++ app/src/main/java/natto/com/oreragorilla/CameraFragment.java
@@ -0,0 +1,46 @@
+package natto.com.oreragorilla;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import natto.com.oreragorilla.databinding.FragmentCameraBinding;
+
+public class CameraFragment extends Fragment {
+
+    FragmentCameraBinding binding;
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_camera, container, false);
+
+        binding.textCamera.setText("ほげ");
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
