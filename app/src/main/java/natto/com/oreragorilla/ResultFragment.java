0c0ae9a5cf925b1ed6f60d82d76987d754a16e99
T.Mazda <inukakinekokaki@gmail.com>
2019-02-12 10:58:13 +0900

    リザルト画面の戻るボタン追加

--- app/src/main/java/natto/com/oreragorilla/ResultFragment.java
+++ app/src/main/java/natto/com/oreragorilla/ResultFragment.java
@@ -1,26 +1,50 @@
 package natto.com.oreragorilla;
 
-
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import natto.com.oreragorilla.databinding.FragmentCameraBinding;
+import androidx.navigation.Navigation;
+import natto.com.oreragorilla.databinding.FragmentResultBinding;
 
 public class ResultFragment extends Fragment {
 
-    public ResultFragment() {
-        FragmentCameraBinding binding;
+    FragmentResultBinding binding;
 
+    public ResultFragment() {
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false);
+
+        binding.back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_resultFragment_to_cameraFragment);
+            }
+        });
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false);
+        return binding.getRoot();
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
     }
 }
